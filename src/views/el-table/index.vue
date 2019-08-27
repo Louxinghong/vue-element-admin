03fd52ea11252b80ab5819b46ce66f574dd7f672
louxinghong <1056868828@qq.com>
2019-08-27 13:58:43 +0800

    update

--- src/views/el-table/index.vue
+++ src/views/el-table/index.vue
@@ -11,17 +11,33 @@
       <el-table-column label="日期" width="120">
         <template slot-scope="scope">{{ scope.row.date }}</template>
       </el-table-column>
-      <el-table-column prop="name" label="姓名" width="120"></el-table-column>
-      <el-table-column prop="address" label="地址" show-overflow-tooltip></el-table-column>
+      <el-table-column label="图片">
+        <template v-slot="{ row }">
+          <img :src="row.picUrl" />
+        </template>
+      </el-table-column>
+      <el-table-column prop="name" label="名称"></el-table-column>
+      <el-table-column prop="place" label="地址"></el-table-column>
+      <el-table-column label="数量">
+        <template v-slot="{ row }">
+          <el-input-number v-model="row.num" @change="handleChangeNums" :min="0" size="small"></el-input-number>
+        </template>
+      </el-table-column>
+      <el-table-column prop="singlePrice" label="单价"></el-table-column>
+      <el-table-column label="总价">
+        <template v-slot="{ row }">
+          <p>{{ row.num * row.singlePrice }}</p>
+        </template>
+      </el-table-column>
       <el-table-column fixed="right" label="操作" width="100">
         <template slot-scope="scope">
           <el-button @click="handleClick(scope.row)" type="text">编辑</el-button>
-          <el-button type="text">删除</el-button>
+          <el-button type="text" @click="handleDelete(scope.$index, tableData)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
-    <el-button @click="toggleSelection([tableData[1], tableData[2]])">切换第二、第三行的选中状态</el-button>
-    <el-button @click="toggleSelection()">取消选择</el-button>
+
+    <p>总价格：{{ allShoppingsPrice }}</p>
   </div>
 </template>
 
@@ -29,46 +45,50 @@
 export default {
   data () {
     return {
-      tableData: [{
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-08',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-07',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }],
-      multipleSelection: []
+      tableData: [
+        {
+          date: '2016-05-03',
+          picUrl: require('../../images/1.png'),
+          name: 'elementUi',
+          place: 'China',
+          num: 3,
+          singlePrice: 1000
+        },
+        {
+          date: '2018-1-1',
+          picUrl: require('../../images/1.png'),
+          name: '啥是佩奇',
+          place: 'China',
+          num: 1,
+          singlePrice: 900
+        }
+      ],
+      multipleSelection: [],
+      allShoppingsPrice: 0,
+      thisPrice: 0,
+      lastChangeNums: 0
     }
   },
 
   methods: {
+    handleChangeNums (val) {
+      this.allShoppingsPrice = 0
+      this.multipleSelection.forEach(item => this.allShoppingsPrice += item.num * item.singlePrice)
+    },
     handleClick (row) {
       console.log(row)
     },
+    handleDelete (index, data) {
+      data.splice(index, 1)
+    },
     handleSelectionChange (val) {
       this.multipleSelection = val
+      this.allShoppingsPrice = 0
+      this.multipleSelection.forEach(item => this.allShoppingsPrice += item.num * item.singlePrice)
     }
   }
 }
 </script>
+
+<style lang="less" scoped>
+</style>
