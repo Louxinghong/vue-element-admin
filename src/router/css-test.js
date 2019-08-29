f11a178f731c1ce3eb75e24411ed70ddd3475c0f
louxinghong <1056868828@qq.com>
2019-08-29 17:58:13 +0800

    新增html测试栏

--- src/router/css-test.js
+++ src/router/css-test.js
@@ -5,15 +5,15 @@ export const routes = [
     path: '/css-test',
     name: 'CssTest',
     component: Layout,
-    redirect: '/css-test/test',
+    redirect: '/css-test/css',
     alwaysShow: false,
     children: [
       {
-        path: 'test',
-        name: 'Test',
+        path: 'css',
+        name: 'Css',
         component: () =>
           import(/*webpackChunkName: 'group-css-test' */ '@/views/css-test/index.vue'),
-        meta: { title: 'css-test', icon: 'css-test' }
+        meta: { title: 'css-test', icon: 'css' }
       }
     ]
   }
---
+++ src/views/html-test/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="content">
+    <p>拖动图片到矩形框中</p>
+    <div class="put-on" @drop="drop($event)" @dragover="allowDrop($event)" width="336" height="69"></div>
+    <img src="../../images/1.png" :draggable="true" @dragstart="drag($event)" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HtmlTest',
+  methods: {
+    allowDrop (ev) {
+      ev.preventDefault()
+    },
+    drag (ev) {
+      ev.dataTransfer.setData('Text', ev.target.id)
+    },
+    drop (ev) {
+      ev.preventDefault()
+      var data = ev.dataTransfer.getData('Text')
+      ev.target.appendChild(document.getElementById(data))
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.put-on {
+  width: 350px;
+  height: 70px;
+  padding: 10px;
+  border: 1px solid #aaaaaa;
+}
+</style>
