1f00395f375072bc578ae0f970ca0fe124a77649
louxinghong <louxinghong@xinyunlian.com>
2019-10-28 15:22:32 +0800

    yarn add -dev prettier-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,21 +3,21 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: ['plugin:vue/essential'],
+  extends: ["plugin:vue/essential"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
-  plugins: ['vue'],
+  plugins: ["vue"],
   rules: {
-    semi: ['error', 'never'], // 行尾不使用分号
-    'no-multi-spaces': 1, // 不能用多余的空格,
-    'space-before-function-paren': [2, 'always'], // 函数定义时括号前面要不要有空格
-    'no-multiple-empty-lines': [2, { max: 1 }], // 空行最多不能超过2行
-    quotes: [2, 'single'], // 引号类型 `` "" ''
-    'no-console': 'off' // 可以使用console.log
+    semi: ["error", "never"], // 行尾不使用分号
+    "no-multi-spaces": 1, // 不能用多余的空格,
+    "space-before-function-paren": [2, "always"], // 函数定义时括号前面要不要有空格
+    "no-multiple-empty-lines": [2, { max: 1 }], // 空行最多不能超过2行
+    quotes: [2, "single"], // 引号类型 `` "" ''
+    "no-console": "off" // 可以使用console.log
   }
-}
+};
