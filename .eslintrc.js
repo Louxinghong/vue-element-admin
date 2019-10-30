d007fe1f62520ad3ef8f08da15315051561a1ec0
louxinghong <louxinghong@xinyunlian.com>
2019-10-30 17:42:03 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,22 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es6: true
+    browser: true
   },
   extends: ["plugin:vue/essential"],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
+    saveAs: true
   },
   parserOptions: {
     parser: "babel-eslint"
   },
-  plugins: ["vue"],
   rules: {
     semi: ["error", "never"], // 行尾不使用分号
     "no-multi-spaces": 1, // 不能用多余的空格,
     "space-before-function-paren": [2, "always"], // 函数定义时括号前面要不要有空格
     "no-multiple-empty-lines": [2, { max: 1 }], // 空行最多不能超过2行
     quotes: [2, "single"], // 引号类型 `` "" ''
-    "no-console": "off" // 可以使用console.log
+    "no-console": "off", // 可以使用console.log
+    "no-return-await": "off"
   }
 };
--- src/layout/components/Navbar/index.vue
+++ src/layout/components/Navbar/index.vue
@@ -3,14 +3,7 @@
     <span class="hiddenornot" @click="changeCollapse">
       <i :class="{'el-icon-s-unfold': isCollapse, 'el-icon-s-fold': !isCollapse}"></i>
     </span>
-    <el-breadcrumb separator="/">
-      <transition-group name="breadcrumb">
-        <el-breadcrumb-item v-for="(breadcrumb, index) in breadcrumbs" :key="breadcrumb.path">
-          <span v-if="index === breadcrumbs.length-1">{{ breadcrumb.meta.title }}</span>
-          <a v-else @click="gotoPath(breadcrumb.path)">{{ breadcrumb.meta.title }}</a>
-        </el-breadcrumb-item>
-      </transition-group>
-    </el-breadcrumb>
+    <Breadcrumb />
     <div class="right-set">
       <Search />
       <Screenfull />
@@ -21,6 +14,7 @@
 
 <script>
 import { mapState, mapActions } from 'vuex'
+import Breadcrumb from './components/Breadcrumb.vue'
 import Search from './components/Search.vue'
 import Screenfull from './components/Screenfull.vue'
 import Setting from './components/Setting.vue'
@@ -28,46 +22,10 @@ import Setting from './components/Setting.vue'
 export default {
   name: 'NavBar',
   components: {
+    Breadcrumb,
     Search,
     Screenfull,
     Setting
-  },
-  data () {
-    return {
-      breadcrumbs: null
-    }
-  },
-  computed: {
-    ...mapState('status', ['isCollapse'])
-  },
-  watch: {
-    $route (route) {
-      this.getroutepath()
-    }
-  },
-  created () {
-    this.getroutepath()
-  },
-  methods: {
-    ...mapActions('status', ['changeCollapse']),
-
-    // 生成面包屑
-    getroutepath () {
-      let matched = this.$route.matched.filter(item => item.name)
-      const firstpath = matched[0]
-      if (firstpath && firstpath.name !== 'Dashboard') {
-        matched = [{ path: '/dashboard', meta: { title: '首页' } }].concat(
-          matched
-        )
-      }
-      this.breadcrumbs = matched.filter(item => item.meta && item.meta.title)
-    },
-    gotoPath (data) {
-      this.$router.push(data)
-    },
-    onChangeSetting () {
-      this.$store.dispatch('status/changeDrawer')
-    }
   }
 }
 </script>
@@ -90,31 +48,6 @@ export default {
   cursor: pointer;
   outline: none;
   font-size: 19px;
-}
-
-.el-breadcrumb {
-  height: @commonheigth;
-  line-height: @commonheigth;
-  font-size: 14px;
-}
-
-.breadcrumb-enter-active,
-.breadcrumb-leave-active {
-  transition: all 0.5s;
-}
-
-.breadcrumb-enter,
-.breadcrumb-leave-active {
-  opacity: 0;
-  transform: translateX(20px);
-}
-
-.breadcrumb-move {
-  transition: all 0.5s;
-}
-
-.breadcrumb-leave-active {
-  position: absolute;
 }
 
 .right-set {
