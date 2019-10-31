81792f830916620236917fbf54b8b7d3f85b7e9a
louxinghong <louxinghong@xinyunlian.com>
2019-10-31 17:04:37 +0800

    update

---
+++ externals.config.js
@@ -0,0 +1,94 @@
+module.exports = {
+  common: [
+    {
+      id: 'normalize.css',
+      assets: '//cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.min.css',
+      global: null
+    },
+    {
+      id: 'vue',
+      assets: '//cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
+      global: 'Vue'
+    },
+    {
+      id: 'vue-router',
+      assets: '//cdn.jsdelivr.net/npm/vue-router@3.1.2/dist/vue-router.min.js',
+      global: 'VueRouter'
+    },
+    {
+      id: 'vuex',
+      assets: '//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
+      global: 'Vuex'
+    },
+    {
+      id: 'flyio',
+      assets: '//cdn.jsdelivr.net/npm/flyio@0.6.14/dist/fly.min.js',
+      global: 'fly'
+    },
+    {
+      id: 'js-cookie',
+      assets: '//cdn.jsdelivr.net/npm/js-cookie@2.2.1/src/js.cookie.min.js',
+      global: 'Cookies'
+    },
+    {
+      id: 'blueimp-md5',
+      assets: '//cdn.jsdelivr.net/npm/blueimp-md5@2.11.1/js/md5.min.js',
+      global: 'md5'
+    },
+    {
+      id: 'dayjs',
+      assets: '//cdn.jsdelivr.net/npm/dayjs@1.8.16/dayjs.min.js',
+      global: 'dayjs'
+    },
+    {
+      id: 'nprogress',
+      assets: [
+        '//cdn.jsdelivr.net/npm/nprogress@0.2.0/nprogress.min.js',
+        '//cdn.jsdelivr.net/npm/nprogress@0.2.0/nprogress.min.css'
+      ],
+      global: 'NProgress'
+    },
+    {
+      id: 'qrious',
+      assets: '//cdn.jsdelivr.net/npm/qrious@4.0.2/dist/qrious.min.js',
+      global: 'QRious'
+    },
+    {
+      id: 'lodash',
+      assets: '//cdn.jsdelivr.net/npm/lodash@4.17.11/lodash.min.js',
+      global: '_'
+    },
+    {
+      id: 'fuse.js',
+      assets: '//cdn.jsdelivr.net/npm/fuse.js@3.4.5/dist/fuse.min.js',
+      global: 'Fuse'
+    },
+    {
+      id: 'viewerjs',
+      assets: [
+        '//cdn.jsdelivr.net/npm/viewerjs@1.3.6/dist/viewer.min.js',
+        '//cdn.jsdelivr.net/npm/viewerjs@1.3.6/dist/viewer.min.css'
+      ],
+      global: 'Viewer'
+    },
+    {
+      id: 'screenfull',
+      assets: '//cdn.jsdelivr.net/npm/screenfull@4.2.1/dist/screenfull.min.js',
+      global: 'screenfull'
+    },
+    {
+      id: 'file-saver',
+      assets: '//cdn.jsdelivr.net/npm/file-saver@2.0.2/dist/FileSaver.min.js',
+      global: null
+    },
+    {
+      id: 'element-ui',
+      assets: [
+        '//cdn.jsdelivr.net/npm/element-ui@2.11.1/lib/index.js',
+        '//cdn.jsdelivr.net/npm/element-ui@2.11.1/lib/theme-chalk/reset.css',
+        '//cdn.jsdelivr.net/npm/element-ui@2.11.1/lib/theme-chalk/index.css'
+      ],
+      global: 'ELEMENT'
+    }
+  ]
+}
